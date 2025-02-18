70ae3cb778fb3b3adb45fd791274c92506388a84
“malcolmmutare” <mutarem@gmail.com>
2025-02-18 18:12:26 +0200

    bugfix: resolved import error

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import { jsx as _jsx, Fragment as _Fragment } from "react/jsx-runtime";
+import './App.css';
+import MatrixRain from './components/MatrixRain';
+function App() {
+    return (_jsx(_Fragment, { children: _jsx(MatrixRain, {}) }));
+}
+export default App;
